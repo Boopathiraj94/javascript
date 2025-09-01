1172ae1dadf343586a0d9227c40ef46adecfa9ca
Boopathiraj94 <boopathirajk55@gmail.com>
2025-09-01 10:57:01 +0530

    again customer load

--- event/register/customers.js
+++ event/register/customers.js
@@ -7,7 +7,7 @@
 //   console.log("customers: ",customers)
 //  let trow ='';
 //   customers.forEach((ele,index)=>{
-    
+
 //            trow +=`<tr>
 //                         <td>${index+1}</td>
 //                         <td>${ele.username}</td>
@@ -17,8 +17,33 @@
 //                             <button>Delete</button>
 //                         </td>
 //                     </tr>`;
-       
-      
+
+
 //   })
 
 //   tbody.innerHTML = trow
+
+
+let customer = JSON.parse(localStorage.getItem("customers"));
+
+console.log(customer)
+
+let tbody = document.querySelector(".customer-table>tbody")
+
+let tableRow = ''
+customer.forEach((ele,index) => {
+    console.log(ele)
+    tableRow += `<tr>
+                        <td>${index+1}</td>
+                        <td>${ele.username}</td>
+                        <td>${ele.email}</td>
+                        <td>
+                            <button>Edit</button>
+                            <button>Delete</button>
+                        </td>
+                    </tr>`;
+
+    // console.log(tableRow)
+})
+
+tbody.innerHTML = tableRow
